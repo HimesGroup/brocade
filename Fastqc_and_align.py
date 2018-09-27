28e37e0c38d289a150a965441058ce9c844997a9
Diwadkar <diwadkar@seas.upenn.edu>
2018-09-27 14:29:37 -0400

    Moved reference genome set-up to README file

--- Fastqc_and_align.py
+++ Fastqc_and_align.py
@@ -3,7 +3,7 @@
 """
 Created on Thu Jun 28 11:21:55 2018
 
-@author: diwadkar
+@author: Avantika Diwadkar (avantika.diwadkar@pennmedicine.upenn.edu)
 """
 
 #Libraries 
@@ -13,18 +13,12 @@ import urllib
 import urllib.request
 from SRAdownload import *
 
+
+#Set up Reference genome files (if not already set up)
+ref_path = = input("Please enter reference genome full path: ")
+
 #Perform FASTQC on the sample files
 fastq_cmd = "fastqc %s/fastq/*.fastq.gz  -o %s/"%(geo_dir,res_dir)
-
-
-#Get human reference genome files -ERROR
-ref_path = '/project/bhimeslab/Reference/hg38/' 
-hg_url = 'http://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz'
-g_cmd = "gunzip hg38.fa.gz"
-
-
-#Build human genome
-bwa_cmd = "bwa index -a %sbwtsw hg38.fa -p hg38"%(ref_path)
 
 #Alignment of reads to genome
 bwa_dir = data_dir + "/bwa"
@@ -40,10 +34,10 @@ bam_dir="%s"
 ls $fastq_dir | sed 's/.fastq.gz//g' > samples.txt
 threat=12
 while read samp; do
-echo "bwa mem -t $threat /project/bhimeslab/Reference/hg38/hg38.fa $fastq_dir/$samp.fastq.gz | samtools view -S -b - | samtools sort -@$threat -T $bam_dir/$samp.tmp -o $bam_dir/$samp.bam -" # alignment and sort$
+echo "bwa mem -t $threat %s/hg38.fa $fastq_dir/$samp.fastq.gz | samtools view -S -b - | samtools sort -@$threat -T $bam_dir/$samp.tmp -o $bam_dir/$samp.bam -" # alignment and sort$
 echo "samtools index $bam_dir/$samp.bam" # index
 done < samples.txt | mk_bsub.py --line 2 --thread 12 --job_name chipseq_bwa
-'''%(fastq_dir,bam_dir)    
+'''%(fastq_dir,bam_dir,ref_path)    
 
 
 if __name__ == '__main__':
@@ -51,15 +45,6 @@ if __name__ == '__main__':
 	#Fastqc on sample files
 	print("Fastqc in progress")
 	#subprocess.call(fastq_cmd,shell=True)
-
-	#Set up hg38  or the required genome if not already present
-	#download_file(hg_url, ref_path)
-	print("Downloaded hg38.fa.gz")
-	#subprocess.call(g_cmd,shell=True)
-	print("Unzipped the file")
-	#subprocess.call(bwa_cmd, shell=True)
-	print("Aligning to genome hg38")
-
 
 	#Make bam and bwa folders in data directory
 	make_dir(bam_dir)    
@@ -69,10 +54,7 @@ if __name__ == '__main__':
 	write_in_file("alignment.sh",Bcall)
 
 	run_cmd = "chmod +x alignment.sh; ./alignment.sh"
-	#subprocess.call(run_cmd, shell=True)
+	subprocess.call(run_cmd, shell=True)
 	print("Alignment file run complete - lsf files created")
 
-	lsf_cmd = "for i in chipseq_bwa*.lsf; do echo $i; bsub < $i; done"
-	#subprocess.call(lsf_cmd, shell=True)
-	print("Jobs submitted")
 
--- README.md
+++ README.md
@@ -45,6 +45,27 @@ brocade is a pipeline to analyze ChIP-seq data using Python scripts and Rmarkdow
   3) GEOID_analysis.Rmd
   4) GEOID_Phenotype_withoutQC.txt
 
-* 
+### Alignment and Quality Control
+* Fastqc_and_align.py script should be used next to get quality control metrics followed by alignment fo the data to the appropriate reference genome. Fastqc is used for quality control while we are using bwa and samtools to get sorted and indexed alignment bam files.
+
+	Set up the hg38 reference genome:
+	> wget http://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz
+
+	> gunzip hg38.fa.gz
+
+	> bwa index -a /ref_path/bwtsw hg38.fa -p hg38 #ref_path should the the path to your reference genome.
+
+	Commands:
+	> python Fastqc_and_align.py #Enter ref_path when prompted
+
+	> for i in chipseq_bwa\*.lsf; do echo $i; bsub < $i; done
+
+	Output files:
+	1) Fastqc folder : fastqc html files for each sample
+	2) bam folder : bam and bam.bai files for each sample
+	3) alignment.sh : To make lsf file for alignment of each sample
+	4) chipseq_bwa\*.lsf : LSF files to run bwa alignment on the reference genome
+
+
 
 
